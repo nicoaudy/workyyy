1ae1c3296875a9e38b00520a7d66726f4d9c554f
Nico Audy <nicoaudy1994@gmail.com>
2020-10-21 15:01:32 +0700

    more designing card

--- src/components/JobCard.svelte
+++ src/components/JobCard.svelte
@@ -2,16 +2,32 @@
   import { onMount } from 'svelte';
 
   export let job;
+  let skills;
+
+  onMount(async () => {
+    skills = job.skills.split(',');
+  });
 </script>
 
 <div class="flex bg-white shadow-md m-4 p-4">
-  <div><img src={job.logo} alt={job.name} /></div>
-  <div class="ml-2">
-    <h3>{job.company}</h3>
-    <h3>{job.position}</h3>
-    <div class="flex">
-      <p class="mr-2">💼 {job.contract}</p>
-      <p>📍 {job.location}</p>
+  <div>
+    <img src={job.logo} alt={job.name} class="h-20 w-20 rounded-full mx-auto" />
+  </div>
+  <div class="flex flex-col justify-between ml-4">
+    <h3 class="font-bold text-purple-500">{job.company}</h3>
+    <h3 class="font-bold text-xl">{job.position}</h3>
+    <div class="flex items-center">
+      <p class="mr-2 text-gray-700">💼 {job.contract}</p>
+      <p class="text-gray-700">📍 {job.location}</p>
     </div>
+  </div>
+  <div class="flex items-center ml-auto">
+    {#if skills}
+      {#each skills as skill}
+        <span
+          class="font-semibold text-xs m-1 p-2 rounded-full text-purple-500 bg-purple-100"
+        >{skill}</span>
+      {/each}
+    {/if}
   </div>
 </div>
